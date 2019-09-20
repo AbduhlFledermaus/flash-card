4339afc12f462fb0ee82ffe3560e9650e37ce44b
AbduhlFledermaus <49551168+AbduhlFledermaus@users.noreply.github.com>
2019-09-20 09:05:58 +0200

    fix bug

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/696836bf-91bb-4e0a-baa6-ecbc1bea0f73/deploy-status)](https://app.netlify.com/sites/flash-cardo/deploys)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-   
+      <h1>FlashCard</h1>
     </div>
   );
 }
