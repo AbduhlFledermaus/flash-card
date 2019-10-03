3d625066b63307d9bd97716bf1f43a6258a16e34
AbduhlFledermaus <49551168+AbduhlFledermaus@users.noreply.github.com>
2019-10-03 15:22:03 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -1,158 +1,78 @@
-import "./App.css";
 import React from "react";
 import firebase from "firebase";
-/*import SignIn from "./components/auth/signIn";*/
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import "./App.css";
 
 function App() {
-	const firebaseConfig = {
-		apiKey: "AIzaSyApLGBjABs8n-4pQD1SmGArIBNJem_wVcU",
-		authDomain: "flashcard-6c43d.firebaseapp.com",
-		databaseURL: "https://flashcard-6c43d.firebaseio.com",
-		projectId: "flashcard-6c43d",
-		storageBucket: "flashcard-6c43d.appspot.com",
-		messagingSenderId: "362798135144",
-		appId: "1:362798135144:web:8b8884570d955c736a08dd"
-	};
+  const firebaseConfig = {
+    apiKey: "AIzaSyApLGBjABs8n-4pQD1SmGArIBNJem_wVcU",
+    authDomain: "flashcard-6c43d.firebaseapp.com",
+    databaseURL: "https://flashcard-6c43d.firebaseio.com",
+    projectId: "flashcard-6c43d",
+    storageBucket: "flashcard-6c43d.appspot.com",
+    messagingSenderId: "362798135144",
+    appId: "1:362798135144:web:8b8884570d955c736a08dd"
+  };
 
-	// Initialize Firebase
-	firebase.initializeApp(firebaseConfig);
-	console.log(firebase);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase);
 
-	const database = firebase.firestore();
-	database
-		.collection("PeriodicTableOfElements")
-		.get()
-		.then(snapshot => {
-			snapshot.docs.forEach(doc => {
-				console.log(doc.data());
-			});
-		});
+  const database = firebase.firestore();
+  /*
+  database
+    .collection("PeriodicTableOfElements")
+    .get()
+    .then(snapshot => {
+      snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+      });
+	});
+	*/
 
-	return (
-		<div className="App">
-			<nav class="light-blue lighten-1" role="navigation">
-				<div class="nav-wrapper container">
-					<a id="logo-container" href="./src/img/flashcard_logo_NEU.png" class="brand-logo"></a>
-					<ul class="right hide-on-med-and-down">
-						<li>
-							<a href="#">Navbar Link</a>
-						</li>
-					</ul>
+  /*const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1)
+    },
+    input: {
+      display: "none"
+    }
+  }));*/
 
-					<ul id="nav-mobile" class="sidenav">
-						<li>
-							<a href="#">Navbar Link</a>
-						</li>
-					</ul>
-					<a href="#" data-target="nav-mobile" class="sidenav-trigger">
-						<i class="material-icons">menu</i>
-					</a>
-				</div>
-			</nav>
-			<div class="section no-pad-bot" id="index-banner">
-				<div class="container">
-					<h1 class="header center orange-text">flashcard</h1>
-					<div class="row center">
-						<h5 class="header col s12 light">Lernen mit virtuellen Kartekarten</h5>
-					</div>
-					<div class="row center">
-						<a
-							href="http://materializecss.com/getting-started.html"
-							id="download-button"
-							class="btn-large waves-effect waves-light orange"
-						>
-							Log In
-						</a>
-						<br></br>
-						<br></br>
-						<a
-							href="http://materializecss.com/getting-started.html"
-							id="download-button"
-							class="btn-large waves-effect waves-light orange"
-						>
-							Sign up
-						</a>
+  return (
+    <div className="App">
+      <div className="topnav" id="myTopnav">
+        <a href="#home" class="active">
+          {" "}
+          Home{" "}
+        </a>
+        <a href="#news">Anmelden</a>
+        <a href="#contact">Registrieren</a>
+        <a href="#about">Logo</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </div>
+      <body>
+        <div className="container">
+          <h1>FLASHCARD</h1>
+        </div>
 
-						<div class="row"></div>
-					</div>
-				</div>
-			</div>
+        <div>
+          <Card className="App-card">
+            <CardContent>Word of the Day</CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </div>
+      </body>
+    </div>
 
-			<div class="container">
-				<div class="section">
-					<div class="row">
-						<div class="col s12 m4">
-							<div class="icon-block">
-								<h2 class="center light-blue-text">
-									<i class="material-icons">schneller...</i>
-								</h2>
-								<h5 class="center">Speeds up development</h5>
-
-								<p class="light">
-									We did most of the heavy lifting for you to provide a default stylings that incorporate our custom
-									components. Additionally, we refined animations and transitions to provide a smoother experience for
-									developers.
-								</p>
-							</div>
-						</div>
-
-						<div class="col s12 m4">
-							<div class="icon-block">
-								<h2 class="center light-blue-text">
-									<i class="material-icons">einfacher...</i>
-								</h2>
-								<h5 class="center">User Experience Focused</h5>
-
-								<p class="light">
-									By utilizing elements and principles of Material Design, we were able to create a framework that
-									incorporates components and animations that provide more feedback to users. Additionally, a single
-									underlying responsive system across all platforms allow for a more unified user experience.
-								</p>
-							</div>
-						</div>
-
-						<div class="col s12 m4">
-							<div class="icon-block">
-								<h2 class="center light-blue-text">
-									<i class="material-icons">nachhaltiger...</i>
-								</h2>
-								<h5 class="center">Sparen Sie Zeit und Papier</h5>
-
-								<p class="light">
-									We have provided detailed documentation as well as specific code examples to help new users get
-									started. We are also always open to feedback and can answer any questions a user may have about
-									Materialize.
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<footer class="page-footer orange">
-				<div class="container">
-					<div class="row">
-						<div class="col l6 s12">
-							<h5 class="white-text">About Flashcard</h5>
-							<p class="grey-text text-lighten-4">Dieses Projekt der Abschlussklasse der LBS 4 entsanden</p>
-						</div>
-						<div class="col l3 s12"></div>
-						<div class="col l3 s12"></div>
-					</div>
-				</div>
-				<div class="footer-copyright">
-					<div class="container">
-						Made by <a class="orange-text text-lighten-3">Simon Hagn</a>
-					</div>
-				</div>
-			</footer>
-
-			<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-			<script src="js/materialize.js"></script>
-			<script src="js/init.js"></script>
-		</div>
-
-		/*<!-- The core Firebase JS SDK is always required and must be listed first -->
+    /*<!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js"></script>
 
 <!-- TODO: Add SDKs for Firebase products that you want to use
@@ -173,7 +93,7 @@ function App() {
   firebase.initializeApp(firebaseConfig);
 </script>
 */
-	);
+  );
 }
 
 export default App;
--- src/common/store.js
+++ src/common/store.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 export const initialState = { user: { loggedIn: false } };
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case 'setUrl':
+    case "setUrl":
       return { user: action.value };
     default:
       return state;
