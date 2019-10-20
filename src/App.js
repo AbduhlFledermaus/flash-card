63aacbcdc589f6ff8d0efc51f108205db6124aa3
AbduhlFledermaus <49551168+AbduhlFledermaus@users.noreply.github.com>
2019-10-21 00:28:54 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ import Routes from "./Routes";
 import "./App.css";
 
 function App() {
-  /**
-   * firebase configuration and keys
-   *
-   */
+	/**
+	 * firebase configuration and keys
+	 *
+	 */
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyApLGBjABs8n-4pQD1SmGArIBNJem_wVcU",
-    authDomain: "flashcard-6c43d.firebaseapp.com",
-    databaseURL: "https://flashcard-6c43d.firebaseio.com",
-    projectId: "flashcard-6c43d",
-    storageBucket: "flashcard-6c43d.appspot.com",
-    messagingSenderId: "362798135144",
-    appId: "1:362798135144:web:8b8884570d955c736a08dd"
-  };
+	const firebaseConfig = {
+		apiKey: "AIzaSyApLGBjABs8n-4pQD1SmGArIBNJem_wVcU",
+		authDomain: "flashcard-6c43d.firebaseapp.com",
+		databaseURL: "https://flashcard-6c43d.firebaseio.com",
+		projectId: "flashcard-6c43d",
+		storageBucket: "flashcard-6c43d.appspot.com",
+		messagingSenderId: "362798135144",
+		appId: "1:362798135144:web:8b8884570d955c736a08dd"
+	};
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+	if (!firebase.apps.length) {
+		firebase.initializeApp(firebaseConfig);
+	}
 
-  const [store, dispatch] = useReducer(reducer, initialState);
+	const [store, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <Context.Provider value={{ store, dispatch }}>
-      <div className="App">
-        <Routes />
-        <EasterEgg />
-      </div>
-    </Context.Provider>
-  );
+	return (
+		<Context.Provider value={{ store, dispatch }}>
+			<div className="App">
+				<Routes />
+				<EasterEgg />
+			</div>
+		</Context.Provider>
+	);
 }
 
 export default App;
--- src/Welcome.js
+++ src/Welcome.js
@@ -6,260 +6,244 @@ import firebase from "firebase";
 
 // Welcome
 export class Welcome extends Component {
-  state = {
-    cards: [],
-    menuOpen: false,
-    currentCard: 0,
-    flipped: false,
-    editing: false,
-    front: "",
-    back: ""
-  };
+	state = {
+		cards: [],
+		menuOpen: false,
+		currentCard: 0,
+		flipped: false,
+		editing: false,
+		front: "",
+		back: ""
+	};
 
-  init = async () => {
-    await firebase
-      .firestore()
-      .collection("user/" + this.props.match.params.id + "/" + "cards")
-      .get()
-      .then(d => {
-        let cds = [];
-        d.forEach(card => {
-          let c = {
-            id: card.id,
-            front: card.data().front,
-            back: card.data().back,
-            correct: false
-          };
-          cds.push(c);
-        });
-        //this.state.cards = cds;
-        this.setState({ cards: [...cds] });
-        //let card = { id: d.id, front, back, correct: false };
-      });
-  };
+	init = async () => {
+		await firebase
+			.firestore()
+			.collection("user/" + this.props.match.params.id + "/" + "cards")
+			.get()
+			.then(d => {
+				let cds = [];
+				d.forEach(card => {
+					let c = {
+						id: card.id,
+						front: card.data().front,
+						back: card.data().back,
+						correct: false
+					};
+					cds.push(c);
+				});
+				//this.state.cards = cds;
+				this.setState({ cards: [...cds] });
+				//let card = { id: d.id, front, back, correct: false };
+			});
+	};
 
-  resetFormInputs = () => {
-    this.setState({
-      front: "",
-      back: ""
-    });
-  };
+	resetFormInputs = () => {
+		this.setState({
+			front: "",
+			back: ""
+		});
+	};
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 
-  // TOGGLE NEW CARD MENU
-  toggleMenu = () => {
-    let { menuOpen } = this.state;
-    this.setState({
-      menuOpen: !menuOpen
-    });
-  };
+	// TOGGLE NEW CARD MENU
+	toggleMenu = () => {
+		let { menuOpen } = this.state;
+		this.setState({
+			menuOpen: !menuOpen
+		});
+	};
 
-  // TOGGLE EDITING MODE
-  toggleEdit = () => {
-    let { cards, currentCard } = this.state;
-    this.resetFormInputs();
-    this.setState({
-      editing: !this.state.editing,
-      front: cards[currentCard].front,
-      back: cards[currentCard].back
-    });
-  };
+	// TOGGLE EDITING MODE
+	toggleEdit = () => {
+		let { cards, currentCard } = this.state;
+		this.resetFormInputs();
+		this.setState({
+			editing: !this.state.editing,
+			front: cards[currentCard].front,
+			back: cards[currentCard].back
+		});
+	};
 
-  // DELETE ALL CARDS FROM ARRAY
-  deleteCards = () => {
-    this.state.cards.forEach(c => {
-      firebase
-        .firestore()
-        .collection("user/" + this.props.match.params.id + "/" + "cards")
-        .doc(c.id)
-        .delete()
-        .then(d => console.log(d));
-    });
-    this.setState({
-      cards: []
-    });
-  };
+	// DELETE ALL CARDS FROM ARRAY
+	deleteCards = () => {
+		this.state.cards.forEach(c => {
+			firebase
+				.firestore()
+				.collection("user/" + this.props.match.params.id + "/" + "cards")
+				.doc(c.id)
+				.delete()
+				.then(d => console.log(d));
+		});
+		this.setState({
+			cards: []
+		});
+	};
 
-  // DELETE A SINGLE CARD
-  deleteCard = id => {
-    let { cards } = this.state;
-    firebase
-      .firestore()
-      .collection("user/" + this.props.match.params.id + "/" + "cards")
-      .doc(id)
-      .delete()
-      .then(d => console.log(d));
+	// DELETE A SINGLE CARD
+	deleteCard = id => {
+		let { cards } = this.state;
+		firebase
+			.firestore()
+			.collection("user/" + this.props.match.params.id + "/" + "cards")
+			.doc(id)
+			.delete()
+			.then(d => console.log(d));
 
-    this.setState({
-      cards: cards.filter(card => {
-        return card.id !== id;
-      })
-    });
-  };
+		this.setState({
+			cards: cards.filter(card => {
+				return card.id !== id;
+			})
+		});
+	};
 
-  // GENERATE RANDOM ID
-  getId = () => {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  };
+	// GENERATE RANDOM ID
+	getId = () => {
+		return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+	};
 
-  //CREATE CARD FOR ARRAY
-  createCard = (front, back) => {
-    let { cards } = this.state;
-    let card = { id: this.getId(), front, back, correct: false };
-    firebase
-      .firestore()
-      .collection("user/" + this.props.match.params.id + "/" + "cards")
-      .add({ front: front, back: back })
-      .then(data => {
-        console.log(data);
-        card.id = data.id;
-      });
-    this.setState({
-      cards: [...cards, card]
-    });
-  };
+	//CREATE CARD FOR ARRAY
+	createCard = (front, back) => {
+		let { cards } = this.state;
+		let card = { id: this.getId(), front, back, correct: false };
+		firebase
+			.firestore()
+			.collection("user/" + this.props.match.params.id + "/" + "cards")
+			.add({ front: front, back: back })
+			.then(data => {
+				console.log(data);
+				card.id = data.id;
+			});
+		this.setState({
+			cards: [...cards, card]
+		});
+	};
 
-  // EDIT CARD IN ARRAY
-  editCard = cardData => {
-    const cards = this.state.cards.map(card => {
-      if (card.id === cardData.id) {
-        firebase
-          .firestore()
-          .collection("user/" + this.props.match.params.id + "/" + "cards")
-          .doc(cardData.id)
-          .update(cardData)
-          .then();
-        return cardData;
-      }
-      return card;
-    });
-    this.setState({ cards: cards });
-  };
+	// EDIT CARD IN ARRAY
+	editCard = cardData => {
+		const cards = this.state.cards.map(card => {
+			if (card.id === cardData.id) {
+				firebase
+					.firestore()
+					.collection("user/" + this.props.match.params.id + "/" + "cards")
+					.doc(cardData.id)
+					.update(cardData)
+					.then();
+				return cardData;
+			}
+			return card;
+		});
+		this.setState({ cards: cards });
+	};
 
-  // CYCLE RIGHT
-  cycleRight = () => {
-    let { currentCard, cards } = this.state;
-    let index = currentCard;
-    if (currentCard >= cards.length - 1) {
-      index = 0;
-    } else {
-      index++;
-    }
-    this.setState({
-      currentCard: index
-    });
-  };
+	// CYCLE RIGHT
+	cycleRight = () => {
+		let { currentCard, cards } = this.state;
+		let index = currentCard;
+		if (currentCard >= cards.length - 1) {
+			index = 0;
+		} else {
+			index++;
+		}
+		this.setState({
+			currentCard: index
+		});
+	};
 
-  //CYCLE LEFT
-  cycleLeft = () => {
-    let { currentCard, cards } = this.state;
-    let index = currentCard;
-    if (currentCard <= 0) {
-      index = cards.length - 1;
-    } else {
-      index--;
-    }
-    this.setState({
-      currentCard: index
-    });
-  };
+	//CYCLE LEFT
+	cycleLeft = () => {
+		let { currentCard, cards } = this.state;
+		let index = currentCard;
+		if (currentCard <= 0) {
+			index = cards.length - 1;
+		} else {
+			index--;
+		}
+		this.setState({
+			currentCard: index
+		});
+	};
 
-  // FLIP CARD
-  flipCard = () => {
-    this.setState({
-      flipped: !this.state.flipped
-    });
-  };
+	// FLIP CARD
+	flipCard = () => {
+		this.setState({
+			flipped: !this.state.flipped
+		});
+	};
 
-  // Render
-  render() {
-    let {
-      cards,
-      currentCard,
-      flipped,
-      front,
-      back,
-      editing,
-      menuOpen
-    } = this.state;
-    this.init();
-    return (
-      <>
-        {menuOpen ? (
-          <div className="overlay">
-            <div className="close-menu" onClick={this.toggleMenu}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                <path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" />
-              </svg>
-            </div>
-            <CardForm
-              reset={this.resetFormInputs}
-              toggleMenu={this.toggleMenu}
-              newCard={this.createCard}
-              front={front}
-              back={back}
-              handleChange={this.handleChange}
-            />
-          </div>
-        ) : editing ? (
-          <div className="overlay">
-            <div className="close-menu" onClick={this.toggleEdit}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                <path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" />
-              </svg>
-            </div>
-            <CardForm
-              cards={cards}
-              currentCard={currentCard}
-              reset={this.resetFormInputs}
-              toggleMenu={this.toggleMenu}
-              newCard={this.createCard}
-              editing={editing}
-              edit={this.editCard}
-              toggleEdit={this.toggleEdit}
-              front={front}
-              back={back}
-              handleChange={this.handleChange}
-            />
-          </div>
-        ) : null}
-        <div className="container">
-          <button className="btn" onClick={this.toggleMenu}>
-            Karte hinzufügen
-          </button>
-          <button className="btn" onClick={this.deleteCards}>
-            Alle Karten verwerfen
-          </button>
-          <button
-            className="btn"
-            onClick={() => {
-              this.props.history.push("/logout");
-            }}
-          >
-            Abmelden
-          </button>
-        </div>
-        <CardList
-          cards={cards}
-          currentCard={currentCard}
-          increase={this.cycleRight}
-          decrease={this.cycleLeft}
-          flipCard={this.flipCard}
-          flipped={flipped}
-          toggle={this.toggleMenu}
-          deleteCard={this.deleteCard}
-          edit={this.toggleEdit}
-        />
-      </>
-    );
-  }
+	// Render
+	render() {
+		let { cards, currentCard, flipped, front, back, editing, menuOpen } = this.state;
+		this.init();
+		return (
+			<>
+				{menuOpen ? (
+					<div className="overlay">
+						<div className="close-menu" onClick={this.toggleMenu}></div>
+						<CardForm
+							reset={this.resetFormInputs}
+							toggleMenu={this.toggleMenu}
+							newCard={this.createCard}
+							front={front}
+							back={back}
+							handleChange={this.handleChange}
+						/>
+					</div>
+				) : editing ? (
+					<div className="overlay">
+						<div className="close-menu" onClick={this.toggleEdit}></div>
+						<CardForm
+							cards={cards}
+							currentCard={currentCard}
+							reset={this.resetFormInputs}
+							toggleMenu={this.toggleMenu}
+							newCard={this.createCard}
+							editing={editing}
+							edit={this.editCard}
+							toggleEdit={this.toggleEdit}
+							front={front}
+							back={back}
+							handleChange={this.handleChange}
+						/>
+					</div>
+				) : null}
+				<div className="container">
+					<button className="btn" onClick={this.toggleMenu}>
+						Karte hinzufügen
+					</button>
+					<button className="btn" onClick={this.deleteCards}>
+						Alle Karten verwerfen
+					</button>
+					<button
+						className="btn"
+						onClick={() => {
+							this.props.history.push("/logout");
+						}}
+					>
+						Abmelden
+					</button>
+				</div>
+				<CardList
+					cards={cards}
+					currentCard={currentCard}
+					increase={this.cycleRight}
+					decrease={this.cycleLeft}
+					flipCard={this.flipCard}
+					flipped={flipped}
+					toggle={this.toggleMenu}
+					deleteCard={this.deleteCard}
+					edit={this.toggleEdit}
+				/>
+			</>
+		);
+	}
 }
 
 export default Welcome;
